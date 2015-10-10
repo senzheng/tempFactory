e5f8d7d6b02fab7530e97adf6d0f41e27a4325ef
szheng <szheng@Xis-MacBook-Air.local>
2015-10-09 19:09:19 -0700

    third commit

--- controller/tempTwo.js
+++ controller/tempTwo.js
@@ -40,12 +40,69 @@ angular.module("tempTwo")
                 return daysEvent
              }
 
+
+
              $scope.todayDay = d.getDate();
              $scope.todayMonth = d.getMonth() + 1;
              $scope.todayYear = d.getFullYear();
+             var Month = d.getMonth();
+             var Year = d.getFullYear();
 
-             $scope.numberOfDay = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();;
+             $scope.numberOfDay = new Date(Year, Month + 1, 0).getDate();
+              
+              $scope.showMonth = function (){
+             	if($scope.todayMonth - 1 == 0){
+                   $scope.Months = "January";
+             	}else if($scope.todayMonth - 1 == 1){
+                   $scope.Months = "February";
+             	}else if($scope.todayMonth - 1 == 2){
+             		$scope.Months = "March";
+             	}else if($scope.todayMonth - 1 == 3){
+             		$scope.Months = "April";
+             	}else if($scope.todayMonth - 1 == 4){
+             		$scope.Months = "May";
+             	}else if($scope.todayMonth - 1 == 5){
+             		$scope.Months = "June";
+             	}else if($scope.todayMonth - 1== 6){
+             		$scope.Months = "July";
+             	}else if($scope.todayMonth - 1== 7){
+             		$scope.Months = "August";
+             	}else if($scope.todayMonth - 1== 8){
+             		$scope.Months = "September";
+             	}else if($scope.todayMonth - 1== 9){
+             		$scope.Months = "October";
+             	}else if($scope.todayMonth - 1== 10){
+             		$scope.Months = "November";
+             	}else{
+             		$scope.Months = "December";
+             	}
+             }
              
+             $scope.getDays = function (index){
+                   var someday = new Date(Year , $scope.todayMonth - 1, index).getDay();
+                     //$scope.dayOne = someday;
+                   if(someday == 0){
+                   	$scope.day = "SUNDAY";
+                   }else if(someday == 1){
+                   	$scope.day = "MONDAY";
+                   }else if(someday == 2){
+                   	$scope.day = "TUESDAY";
+                   }else if(someday == 3){
+                   	$scope.day = "WEDNESDAY"
+                   }else if(someday == 4){
+                   	$scope.day = "THURSDAY";
+                   }else if(someday == 5){
+                   	$scope.day = "Friday";
+                   }else if(someday == 6){
+                   	$scope.day = "SATURDAY";
+                   }
+             }
+             
+            $scope.lastOrNext = function (index) {
+                    $scope.numberOfDay = new Date(Year, index + 1, 0).getDate();
+                    $scope.todayMonth = index + 1;
+            }
+
              //Function range will return a list or array of numbers
              $scope.range = function(count){
 
